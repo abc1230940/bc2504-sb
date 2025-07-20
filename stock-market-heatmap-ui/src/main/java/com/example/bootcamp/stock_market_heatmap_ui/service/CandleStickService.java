c5d07d2fa66b73c95812d72994bca0471ebd9345
Clarence <abc1230940@yahoo.com.hk>
2025-07-20 16:56:36 +0800

    Delete stock-market-heatmap-ui directory

--- stock-market-heatmap-ui/src/main/java/com/example/bootcamp/stock_market_heatmap_ui/service/CandleStickService.java
+++
@@ -1,8 +0,0 @@
-package com.example.bootcamp.stock_market_heatmap_ui.service;
-
-import java.util.List;
-import com.example.bootcamp.stock_market_heatmap_ui.model.dto.CandleStickDTO;
-
-public interface CandleStickService {
-  List<CandleStickDTO> fetchHistoryData(String symbol);
-}
--- stock-market-heatmap-ui/src/main/java/com/example/bootcamp/stock_market_heatmap_ui/service/HeatmapService.java
+++
@@ -1,9 +0,0 @@
-package com.example.bootcamp.stock_market_heatmap_ui.service;
-
-import java.util.List;
-import com.example.bootcamp.stock_market_heatmap_ui.model.dto.HeatmapDTO;
-
-public interface HeatmapService {
-
-  List<HeatmapDTO> fetchStockData();
-}
