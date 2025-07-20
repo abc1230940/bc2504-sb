c674cbd7fc5055d84694ab42f8218de47123cf0b
Clarence <abc1230940@yahoo.com.hk>
2025-07-20 16:56:46 +0800

    Delete stock-market-project directory

--- stock-market-project/src/main/java/com/example/bootcamp/stock_market_project/service/StockService.java
+++
@@ -1,9 +0,0 @@
-package com.example.bootcamp.stock_market_project.service;
-
-import java.util.List;
-import com.example.bootcamp.stock_market_project.model.dto.StockDTO;
-
-public interface StockService {
-
-  List<StockDTO> getAllStockData();
-}
