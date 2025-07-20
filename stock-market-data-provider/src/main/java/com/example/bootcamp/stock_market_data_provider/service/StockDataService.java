07db75fbb5587b0ac9d344867c8ea9f3b2fb7df2
Clarence <abc1230940@yahoo.com.hk>
2025-07-20 16:56:25 +0800

    Delete stock-market-data-provider directory

--- stock-market-data-provider/src/main/java/com/example/bootcamp/stock_market_data_provider/service/StockDataService.java
+++
@@ -1,8 +0,0 @@
-package com.example.bootcamp.stock_market_data_provider.service;
-
-import com.example.bootcamp.stock_market_data_provider.model.dto.StockDataDTO;
-
-public interface StockDataService {
-
-  StockDataDTO getStockData(String symbol);
-}
